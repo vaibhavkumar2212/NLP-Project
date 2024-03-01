aec7e5fe5a4c6fa6f87cc9b10853b695243e5a29
Vaibhav Kumar <vaibhavkumar2212@gmail.com>
2024-03-01 10:32:45 +0530

    Setup file Added

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Text-Summarizer-Project"
+REPO_NAME = "NLP-Project"
 AUTHOR_USER_NAME = "Vaibhav Kumar"
 SRC_REPO = "textSummarizer"
 AUTHOR_EMAIL = "vaibhavkumar2212@gmail.com"
